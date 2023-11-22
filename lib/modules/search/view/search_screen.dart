255ad0fa8630e425d1f108fc5f7d9d97c52d97c2
khangpham202 <doangiang2612@gmail.com>
2023-11-22 11:35:14 +0700

    update UI

--- lib/modules/search/view/search_screen.dart
+++ lib/modules/search/view/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +7,87 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('HomeScreen'),
-    );
+        body: SafeArea(
+          child: Column(children: [
+            Container(
+              color: Color.fromRGBO(18, 55, 149, 0.914),
+              height: MediaQuery.of(context).size.height / 11,
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 450,
+                      height: 35,
+                      child: TextField(
+                          decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        prefixIcon: Icon(Icons.search),
+                        hintText: 'Search any words',
+                        contentPadding: EdgeInsets.symmetric(vertical: 10),
+                        border: OutlineInputBorder(),
+                      )),
+                    ),
+                    Gap(5),
+                    SizedBox(
+                      child: GestureDetector(
+                        onTap: () {
+                          print(123);
+                          Scaffold.of(context).openDrawer();
+                        },
+                        child: Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color.fromARGB(255, 19, 21, 123),
+                            ),
+                            padding: EdgeInsets.all(5),
+                            child: Text(
+                              'EV',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+                child: Container(
+              color: Colors.red,
+            ))
+          ]),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  'Sidebar Header',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text('Item 1'),
+                onTap: () {
+                  // Handle item 1 tap
+                },
+              ),
+
+              // Add more list items as needed
+            ],
+          ),
+        ));
   }
 }
--- lib/modules/translatePage/view/translate_screen.dart
+++ lib/modules/translatePage/view/translate_screen.dart
@@ -23,135 +23,137 @@ class _TranslateScreenState extends State<TranslateScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            color: Color.fromRGBO(18, 55, 149, 0.914),
-            height: MediaQuery.of(context).size.height / 11,
-            child: Center(
-                child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text(text1,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                        )),
-                    Text(text2,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                        )),
-                  ],
-                ),
-                GestureDetector(
-                  onTap: swapLanguage,
-                  child: Icon(
-                    Icons.sync,
-                    color: Colors.white,
-                    size: 30,
-                  ),
-                ),
-              ],
-            )),
-          ),
-          Expanded(
-            child: Column(children: [
-              Gap(30),
-              Text(
-                'Vietnamese to English',
-                style: TextStyle(color: Colors.grey.shade700, fontSize: 15),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: Row(
-                  children: [
-                    Image.asset(
-                      'assets/image/logo/vn_logo.png',
-                      width: 25,
-                      height: 25,
-                    ),
-                    Gap(10),
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade200,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Text('Làm ơn',
-                          style: TextStyle(
-                            color: Color.fromRGBO(18, 55, 149, 0.914),
-                            fontSize: 16,
-                          )),
-                    ),
-                    Gap(10),
-                    GestureDetector(
-                      onTap: null,
-                      child: Icon(
-                        Icons.volume_up_outlined,
-                        color: Colors.grey.shade400,
-                        size: 28,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: Row(
-                  children: [
-                    Image.asset(
-                      'assets/image/logo/uk_logo.png',
-                      // fit: BoxFit.cover,
-                      width: 25,
-                      height: 25,
-                    ),
-                    Gap(10),
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Color.fromRGBO(18, 55, 149, 0.914),
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Text('Please',
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              color: Color.fromRGBO(18, 55, 149, 0.914),
+              height: MediaQuery.of(context).size.height / 12,
+              child: Center(
+                  child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(text1,
                           style: TextStyle(
                             color: Colors.white,
-                            fontSize: 16,
+                            fontSize: 20,
                           )),
+                      Text(text2,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          )),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: swapLanguage,
+                    child: Icon(
+                      Icons.sync,
+                      color: Colors.white,
+                      size: 30,
                     ),
-                    Gap(10),
-                    GestureDetector(
-                      onTap: null,
-                      child: Icon(
-                        Icons.volume_up_outlined,
-                        color: Colors.grey.shade400,
-                        size: 28,
-                      ),
-                    ),
-                  ],
+                  ),
+                ],
+              )),
+            ),
+            Expanded(
+              child: Column(children: [
+                Gap(30),
+                Text(
+                  'Vietnamese to English',
+                  style: TextStyle(color: Colors.grey.shade700, fontSize: 15),
                 ),
-              ),
-            ]),
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Type text or phase',
-              border: OutlineInputBorder(),
-              suffixIcon: GestureDetector(
-                onTap: () {
-                  print(text1);
-                },
-                child: Icon(
-                  Icons.send,
-                  color: Color.fromARGB(219, 39, 39, 221),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        'assets/image/logo/vn_logo.png',
+                        width: 25,
+                        height: 25,
+                      ),
+                      Gap(10),
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade200,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Text('Làm ơn',
+                            style: TextStyle(
+                              color: Color.fromRGBO(18, 55, 149, 0.914),
+                              fontSize: 16,
+                            )),
+                      ),
+                      Gap(10),
+                      GestureDetector(
+                        onTap: null,
+                        child: Icon(
+                          Icons.volume_up_outlined,
+                          color: Colors.grey.shade400,
+                          size: 28,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        'assets/image/logo/uk_logo.png',
+                        // fit: BoxFit.cover,
+                        width: 25,
+                        height: 25,
+                      ),
+                      Gap(10),
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(18, 55, 149, 0.914),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Text('Please',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            )),
+                      ),
+                      Gap(10),
+                      GestureDetector(
+                        onTap: null,
+                        child: Icon(
+                          Icons.volume_up_outlined,
+                          color: Colors.grey.shade400,
+                          size: 28,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ]),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Type text or phase',
+                border: OutlineInputBorder(),
+                suffixIcon: GestureDetector(
+                  onTap: () {
+                    print(text1);
+                  },
+                  child: Icon(
+                    Icons.send,
+                    color: Color.fromARGB(219, 39, 39, 221),
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
