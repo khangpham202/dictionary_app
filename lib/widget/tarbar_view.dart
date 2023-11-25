14e4375927826b98dd4fb85f9a8afcf70e22a404
khangpham202 <doangiang2612@gmail.com>
2023-11-25 11:43:59 +0700

    refractor

--- lib/widget/tarbar_view.dart
+++ lib/widget/tarbar_view.dart
@@ -1,12 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
-class WordMeaningWidget extends StatelessWidget {
-  const WordMeaningWidget({super.key});
+class WordMeaningWidget extends StatefulWidget {
+  final String word;
+  const WordMeaningWidget({super.key, required this.word});
 
   @override
+  State<WordMeaningWidget> createState() => _WordMeaningWidgetState();
+}
+
+class _WordMeaningWidgetState extends State<WordMeaningWidget> {
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("It's cloudy here"),
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'idget.word',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: null,
+                      child: Icon(Icons.note_alt_sharp),
+                    ),
+                    GestureDetector(
+                      onTap: null,
+                      child: Icon(Icons.save),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Gap(10),
+          Row(
+            children: [
+              Text(
+                '/adsd/',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 185, 178, 178),
+                    fontWeight: FontWeight.w700,
+                    fontSize: 18),
+              ),
+              GestureDetector(
+                onTap: null,
+                child: Icon(
+                  Icons.volume_up_outlined,
+                  color: Color.fromRGBO(99, 115, 156, 0.914),
+                  size: 25,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
