26e149ec7c7de58fdd266169a67d61ec392575ba
khangpham202 <doangiang2612@gmail.com>
2023-12-03 00:02:17 +0700

    refractor variables

--- lib/core/common/model/translation.dart
+++ lib/core/common/model/translation.dart
@@ -1,9 +1,16 @@
+import 'package:flutter/widgets.dart';
+
 class Translation {
   final String originalSentence;
-  final Future<String> translatedSentence;
-  final String sourceCountryLogoSrc;
-  final String targetCountryLogoSrc;
+  final String translatedSentence;
+  final Image sourceCountryLogoSrc;
+  final Image targetCountryLogoSrc;
   final String translateFlow;
-  Translation(this.originalSentence, this.translatedSentence,
-      this.sourceCountryLogoSrc, this.targetCountryLogoSrc, this.translateFlow);
+  Translation(
+    this.originalSentence,
+    this.translatedSentence,
+    this.sourceCountryLogoSrc,
+    this.targetCountryLogoSrc,
+    this.translateFlow,
+  );
 }
