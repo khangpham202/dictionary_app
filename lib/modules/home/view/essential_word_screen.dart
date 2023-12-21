bf978d062f4de8642b90e3d167debb5c592adb87
khangpham202 <doangiang2612@gmail.com>
2023-12-21 23:34:27 +0700

    update UI

--- lib/modules/home/view/essential_word_screen.dart
+++ lib/modules/home/view/essential_word_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:training/util/data_service.dart';
@@ -7,23 +5,6 @@ import 'package:training/util/speech.dart';
 
 class EssentialWordScreen extends StatelessWidget {
   const EssentialWordScreen({super.key});
-
-  Color getRandomColor() {
-    Random random = Random();
-    final baseColor = Color.fromARGB(
-      255,
-      random.nextInt(256),
-      random.nextInt(256),
-      random.nextInt(256),
-    );
-    final color = Color.fromARGB(
-      255,
-      (baseColor.red * 0.8).toInt(),
-      (baseColor.green * 0.8).toInt(),
-      (baseColor.blue * 0.8).toInt(),
-    );
-    return color;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,69 +14,59 @@ class EssentialWordScreen extends StatelessWidget {
           centerTitle: true,
         ),
         body: FutureBuilder<Map<String, List<Map<String, dynamic>>>>(
-          future: loadVocabulary(),
+          future: getEssentialWord(),
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return FutureBuilder<Map<String, List<Map<String, dynamic>>>>(
-                future: loadVocabulary(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    Map<String, List<Map<String, dynamic>>> vocabulary =
-                        snapshot.data!;
-                    return ListView.builder(
-                      itemCount: vocabulary.length,
-                      itemBuilder: (context, index) {
-                        final topic = vocabulary.keys.elementAt(index);
-                        final items = vocabulary[topic]!;
+              Map<String, List<Map<String, dynamic>>> vocabulary =
+                  snapshot.data!;
+              return ListView.builder(
+                itemCount: vocabulary.length,
+                itemBuilder: (context, index) {
+                  final topic = vocabulary.keys.elementAt(index);
+                  final items = vocabulary[topic]!;
 
-                        return ExpansionTile(
-                          title: Text(topic),
-                          children: items.map((item) {
-                            return Column(
+                  return ExpansionTile(
+                    title: Text("Subject: $topic"),
+                    children: items.map((item) {
+                      return Column(
+                        children: [
+                          ListTile(
+                            title: Row(
                               children: [
-                                ListTile(
-                                  title: Row(
-                                    children: [
-                                      Text(item['english']),
-                                      GestureDetector(
-                                        onTap: () {
-                                          TextToSpeechService()
-                                              .playTts('en', item['english']);
-                                        },
-                                        child: Icon(Icons.volume_up),
-                                      )
-                                    ],
-                                  ),
-                                  subtitle: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Gap(2),
-                                      Text(item['phonetic']),
-                                      Gap(2),
-                                      Row(
-                                        children: [
-                                          Icon(Icons.arrow_right),
-                                          Text(item['vietnamese']),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Divider(),
+                                Text(item['english']),
+                                GestureDetector(
+                                  onTap: () {
+                                    TextToSpeechService()
+                                        .playTts('en', item['english']);
+                                  },
+                                  child: Icon(Icons.volume_up),
+                                )
                               ],
-                            );
-                          }).toList(),
-                        );
-                      },
-                    );
-                  } else if (snapshot.hasError) {
-                    return Text('Error loading vocabulary');
-                  } else {
-                    return CircularProgressIndicator();
-                  }
+                            ),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Gap(2),
+                                Text(item['phonetic']),
+                                Gap(2),
+                                Row(
+                                  children: [
+                                    Icon(Icons.arrow_right),
+                                    Text(item['vietnamese']),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          Divider(),
+                        ],
+                      );
+                    }).toList(),
+                  );
                 },
               );
+            } else if (snapshot.hasError) {
+              return Text('Error loading vocabulary');
             } else {
               return CircularProgressIndicator();
             }
