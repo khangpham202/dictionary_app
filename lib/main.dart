9837414970aa42302f067b172562a91d1760f3d2
khangpham202 <doangiang2612@gmail.com>
2024-01-06 10:05:16 +0700

    update : BlocProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:training/modules/home/view/essential_word_screen.dart';
-// import 'package:training/components/tarbar_view.dart';
-import 'package:training/modules/intro/note.dart';
-import 'package:training/modules/intro/welcome_screen.dart';
-import 'package:training/modules/translatePage/bloc/language/language_bloc.dart';
-import 'package:training/components/navigation.dart';
-import 'firebase_options.dart';
+import 'package:training/bloc_provider_scope.dart';
+import 'package:training/core/router/route_configurations.dart';
+import 'package:training/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+
+  runApp(
+    const BlocProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -22,17 +22,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      // home: BlocProvider(
-      //   create: (context) => LanguageBloc(),
-      //   child: NavigationBottomBar(
-      //     indexScreen: 0,
-      //   ),
-      // ),
-      home: WelcomeScreen(),
-
-      // home: WelcomeScreen(),
+      routerConfig: routerConfig,
     );
   }
 }
