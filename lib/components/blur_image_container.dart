6c28643c0d859dee78c481e00d911934cb813bb9
khangpham202 <doangiang2612@gmail.com>
2024-01-06 10:24:51 +0700

    Merge branch 'khang1'

--- lib/components/blur_image_container.dart
+++ lib/components/blur_image_container.dart
@@ -28,13 +28,14 @@ class BlurredImageContainer extends StatelessWidget {
                 child: BackdropFilter(
                   filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
                   child: Container(
-                    color: const Color.fromARGB(255, 98, 88, 204).withOpacity(0.1),
+                    color:
+                        const Color.fromARGB(255, 98, 88, 204).withOpacity(0.1),
                     child: Center(
                       child: Text(
                         text,
                         style: TextStyle(
                           color: const Color.fromARGB(255, 248, 248, 248),
-                          fontSize: 28, 
+                          fontSize: 28,
                           fontWeight: FontWeight.bold,
                           shadows: [
                             Shadow(
@@ -44,61 +45,6 @@ class BlurredImageContainer extends StatelessWidget {
                             ),
                           ],
                         ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ));
-  }
-}
-import 'dart:ui';
-
-import 'package:flutter/material.dart';
-
-class BlurredImageContainer extends StatelessWidget {
-  final VoidCallback onTap;
-  final String imagePath;
-  final String text;
-
-  const BlurredImageContainer({
-    super.key,
-    required this.onTap,
-    required this.imagePath,
-    required this.text,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-        onTap: onTap,
-        child: SizedBox(
-          height: 100,
-          child: Stack(
-            fit: StackFit.expand,
-            children: [
-              Image.asset(imagePath, fit: BoxFit.cover),
-              ClipRRect(
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
-                  child: Container(
-                    color: Color.fromARGB(255, 98, 88, 204).withOpacity(0.1),
-                    alignment: Alignment.center,
-                    child: Text(
-                      text,
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 248, 248, 248),
-                        fontSize: 28, // Adjust the font size
-                        fontWeight: FontWeight.bold,
-                        shadows: [
-                          Shadow(
-                            blurRadius: 5,
-                            color: Colors.black.withOpacity(0.7),
-                            offset: Offset(1, 1),
-                          ),
-                        ],
                       ),
                     ),
                   ),
