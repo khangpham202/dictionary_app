3a7305c6229b48200d81d14f0b8c659eddb2b78a
khangpham202 <doangiang2612@gmail.com>
2024-02-27 17:07:42 +0700

    update: initializer,EasyLocalization

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,29 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter/locales.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:training/bloc_provider_scope.dart';
 import 'package:training/core/common/theme/theme_config.dart';
+import 'package:training/core/enum/locales.dart';
 import 'package:training/core/router/route_configurations.dart';
 import 'package:training/firebase_options.dart';
-import 'package:training/modules/setting/bloc/theme/theme_bloc.dart';
+import 'modules/setting/bloc/theme/theme_bloc.dart';
+part 'initializer.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  FirebaseAuth.instance.authStateChanges().listen((User? user) {
-    routerConfig.refresh();
-  });
+  await _Initializer.start();
+
   runApp(
-    const BlocProviderScope(
-      child: MyApp(),
+    EasyLocalization(
+      supportedLocales: [AppLocales.en.locale, AppLocales.vi.locale],
+      path: AppLocales.path,
+      fallbackLocale: AppLocales.en.locale,
+      child: const BlocProviderScope(
+        child: MyApp(),
+      ),
     ),
   );
 }
@@ -28,21 +33,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ThemeBloc, ThemeState>(
-      builder: (context, state) {
-        return MaterialApp.router(
-          theme: ThemeData(
-            colorScheme: ThemeDataStyle.lightColorScheme,
-          ),
-          darkTheme: ThemeData(
-            brightness: Brightness.dark,
-            colorScheme: ThemeDataStyle.darkColorScheme,
-          ),
-          themeMode: state.themeMode,
-          debugShowCheckedModeBanner: false,
-          routerConfig: routerConfig,
-        );
-      },
+    return MaterialApp.router(
+      // theme
+      theme: ThemeData(
+        colorScheme: ThemeDataStyle.lightColorScheme,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ThemeDataStyle.darkColorScheme,
+      ),
+      themeMode: context.watch<ThemeBloc>().state.themeMode,
+      debugShowCheckedModeBanner: false,
+
+      // localization
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+
+      // router
+      routerConfig: routerConfig,
     );
   }
 }
