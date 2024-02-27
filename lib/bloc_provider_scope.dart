a8236126dd7f1e4b6a09c570bd534287bc55f649
khangpham202 <doangiang2612@gmail.com>
2024-02-27 17:10:00 +0700

    update: InitialThemeSetEvent

--- lib/bloc_provider_scope.dart
+++ lib/bloc_provider_scope.dart
@@ -4,6 +4,8 @@ import 'package:training/modules/auth/bloc/authentication_bloc.dart';
 import 'package:training/modules/setting/bloc/theme/theme_bloc.dart';
 import 'package:training/modules/translatePage/bloc/country/country_bloc.dart';
 
+import 'modules/setting/bloc/language/language_bloc.dart';
+
 class BlocProviderScope extends StatelessWidget {
   final Widget child;
   const BlocProviderScope({super.key, required this.child});
@@ -12,7 +14,10 @@ class BlocProviderScope extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(providers: [
       BlocProvider<CountryBloc>(create: (context) => CountryBloc()),
-      BlocProvider<ThemeBloc>(create: (context) => ThemeBloc()),
+      BlocProvider<ThemeBloc>(
+          create: (context) => ThemeBloc()..add(InitialThemeSetEvent())),
+      BlocProvider<CountryLanguageBloc>(
+          create: (context) => CountryLanguageBloc()),
       BlocProvider<AuthenticationBloc>(
           create: (context) => AuthenticationBloc()),
     ], child: child);
