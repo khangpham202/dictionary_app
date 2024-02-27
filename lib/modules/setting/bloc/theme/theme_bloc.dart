3834a04458547f1adc06c466326f12417ea55226
khangpham202 <doangiang2612@gmail.com>
2024-02-27 16:54:07 +0700

    update: bracket

--- lib/modules/setting/bloc/theme/theme_bloc.dart
+++ lib/modules/setting/bloc/theme/theme_bloc.dart
@@ -1,33 +1,29 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
-import 'package:training/util/theme_helper.dart';
-
-
+import 'package:training/util/shared_preferences_helper.dart';
 part 'theme_event.dart';
 part 'theme_state.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
   ThemeBloc() : super(const ThemeInitial(ThemeMode.light)) {
-    on<ThemeEvent>(((event, emit) {
-      //when app is started
-      on<InitialThemeSetEvent>((event, emit) async {
-        final bool hasDarkTheme = await isDark();
-        if (hasDarkTheme) {
-          emit(const ThemeState(ThemeMode.dark));
-        } else {
-          emit(const ThemeState(ThemeMode.light));
-        }
-      });
+    //when app is started
+    on<InitialThemeSetEvent>((event, emit) async {
+      final bool hasDarkTheme = await isDark();
+      if (hasDarkTheme) {
+        emit(const ThemeState(ThemeMode.dark));
+      } else {
+        emit(const ThemeState(ThemeMode.light));
+      }
+    });
 
-      //while switch is clicked
-      on<ThemeSwitchEvent>((event, emit) {
-        final isDark = state.themeMode == ThemeMode.dark;
-        emit(isDark
-            ? const ThemeState(ThemeMode.light)
-            : const ThemeState(ThemeMode.dark));
-        setTheme(isDark);
-      });
-    }));
+    //while switch is clicked
+    on<ThemeSwitchEvent>((event, emit) {
+      final isDark = state.themeMode == ThemeMode.dark;
+      emit(isDark
+          ? const ThemeState(ThemeMode.light)
+          : const ThemeState(ThemeMode.dark));
+      setTheme(isDark);
+    });
   }
 }
